cff1bcfb33b237743913b7465286dafa6328c480
Daniel L. Branco <danlb48986@gmail.com>
2017-10-14 05:50:22 -0500

    Add files via upload

--- csharp/raindrops/Raindrops.cs
+++ csharp/raindrops/Raindrops.cs
@@ -5,10 +5,8 @@ public static class Raindrops
     public static string Convert(int number)
     {
         string message = "";
-        /*Tests if the 'number' has:
-         * 3 and/or 5 and/or 7 as a factor*/
-        //I don't use 'else if' to guarantee 
-        //that all tests be done. 
+        /*Tests if the 'number' has: 3 and/or 5 and/or 7 as a factor*/
+        //I don't use 'else if' to guarantee that all 3 tests be done for 'number'. 
         if (number % 3 == 0)
         {
             message = "Pling";
