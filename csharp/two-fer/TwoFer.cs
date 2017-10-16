a1bf95adabe64fb9ab0f02a6d261b330a18e774c
Daniel L. Branco <danlb48986@gmail.com>
2017-10-15 22:21:39 -0500

    Two-Fer problem

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string Name(string input = null)
+      {
+        string x;
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            x = "you";
+        }
+        else
+        {
+            x = input.Trim();
+        }
+
+        return "One for " + x + ", one for me.";
+      }
+  }
