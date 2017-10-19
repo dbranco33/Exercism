00e5ce96037b089ef273b093b3f032f26df440ad
Daniel L. Branco <danlb48986@gmail.com>
2017-10-19 16:59:25 -0500

    Update Accumulate.cs

--- csharp/accumulate/Accumulate.cs
+++ csharp/accumulate/Accumulate.cs
@@ -6,7 +6,6 @@ public static class AccumulateExtensions
 {
     public static IEnumerable<U> Accumulate<T, U>(this IEnumerable<T> collection, Func<T, U> func)
     {
-        List<T> list = new List<T>();
         foreach(var element in collection)
         {
             //'yield' keyword to return all values of the collection
