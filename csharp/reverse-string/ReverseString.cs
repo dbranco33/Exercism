7f96b416f3490cfc5e4162e1ddce130d2a936146
Daniel L. Branco <danlb48986@gmail.com>
2018-02-08 01:18:12 -0600

    Add files via upload

---
+++ csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        string reverseWord = "";//will hold the reverse input.
+        string[] myArray = new string[input.Length];//will hold the characters in 'input'.
+        int counter = 0;
+
+        //fills the 'myArray' with the 'input' characters.
+        foreach (var character in input)
+        {
+            myArray[counter] = character.ToString();
+            counter++;
+        }
+
+        //build the reverseWord string with the last to 
+        //the first element at myArray.
+        for (int i = myArray.Length; i > 0; i--)
+        {
+            reverseWord += myArray[i - 1];
+        }
+
+        //returns a string that's the inverse of 'input'.
+        return reverseWord;
+    }
+}
